25f6de35219b02c0b8d462be40027aa95941a78f
unknown <mmohamedabdelm@gmail.com>
2024-01-03 21:55:43 +0200

    add file analysis for DB, and make models DB in models.py file for Category, courses, reviews & create superuser in django

---
+++ analysis.md
@@ -0,0 +1,21 @@
+models 
+
+ - Category
+  - name 
+-----------------------------------------------------------
+ - Courses
+   - name 
+   - sub_title
+   - description
+   - price
+   - category
+   - reviews
+-----------------------------------------------------------
+ - Reviews
+  - Course --- fk
+  - user --- fk
+  - review 
+  - rate 
+  - created_at 
+ -----------------------------------------------------------
+
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class Courses(models.Model):
+    name = models.CharField(max_length=20)
+    sub_title = models.TextField(max_length=1000)
+    description= models.TextField(max_length=2500)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, related_name='category_course', on_delete= models.SET_NULL, null=True)
+    def __str__(self):
+        return self.name
+    
+
+class Reviews(models.Model):
+    user = models.ForeignKey(User, related_name='user_review', on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey(Courses, related_name='courses_review', on_delete=models.CASCADE)
+    review = models.TextField(max_length=500)
+    rate = models.IntegerField(5)
+    created_at = models.DateTimeField(timezone.now)
+
+    def __str__(self):
+        return f'{self.user} writing review {self.review[0:50]} for course {self.course}'
+    
