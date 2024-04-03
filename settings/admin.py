fe90ccc7600bdc78c8652f9771bd384c6ae8009a
unknown <mmohamedabdelm@gmail.com>
2024-04-03 02:33:59 +0200

    add models for Company

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Company
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+class About_us(SummernoteModelAdmin):  
+    summernote_fields = '__all__'
+
+admin.site.register(Company, About_us)
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='company/logos/')
+    carousel_image1 = models.ImageField(upload_to='company/carousel/')
+    carousel_image2 = models.ImageField(upload_to='company/carousel/')
+    description = models.TextField()
+    about_us = models.TextField()  # Consider using a WYSIWYG editor like Summernote
+    phone1 = models.CharField(max_length=20)
+    phone2 = models.CharField(max_length=20, blank=True, null=True)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField()
+    social_media_facebook = models.URLField(blank=True, null=True)
+    social_media_whatsapp = models.URLField(blank=True, null=True)
+    social_media_youtube = models.URLField(blank=True, null=True)
+    social_media_other_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
