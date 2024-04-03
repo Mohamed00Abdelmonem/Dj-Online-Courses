76c34fcc1aad36b9571fbc5262b41fedd0cd685c
unknown <mmohamedabdelm@gmail.com>
2024-04-03 23:01:56 +0200

    complated course detail

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Course_Grid, Course_List, Course_Detial
+from .views import Course_Grid, Course_List, Course_Detail
 
 urlpatterns = [
     path('', Course_Grid.as_view()),
     path('/list', Course_List.as_view()),
-    path('/<int:pk>', Course_Detial.as_view()),
+    path('/<int:pk>', Course_Detail.as_view()),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Course, Review
 from django.views.generic import ListView, DetailView
@@ -17,7 +19,13 @@ class Course_List(ListView):
     context_object_name = 'courses'
 
 
-class Course_Detial(DetailView):
+class Course_Detail(DetailView):
     model = Course
     template_name = 'course-details.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_course = self.get_object()
+        related_courses = Course.objects.filter(tags__in=current_course.tags.all()).exclude(id=current_course.id).distinct()
+        context["related_courses"] = related_courses
+        return context
