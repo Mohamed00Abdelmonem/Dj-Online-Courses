4c8df51ecfd097f3fdb74783646229abfd248cc3
unknown <mmohamedabdelm@gmail.com>
2024-04-04 22:16:50 +0200

    fix urls

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import Course_Grid, Course_List, Course_Detail
 
+
+app_name = 'courses'
+
 urlpatterns = [
     path('', Course_Grid.as_view()),
-    path('/list', Course_List.as_view()),
-    path('/<int:pk>', Course_Detail.as_view()),
+    path('list', Course_List.as_view()),
+    path('<int:pk>', Course_Detail.as_view()),
 ]
--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('settings.urls')),
-    path('courses', include('courses.urls')),
+    path('courses/', include('courses.urls', namespace='courses')),
+
+
+
     path('summernote/', include('django_summernote.urls')),
 
 
