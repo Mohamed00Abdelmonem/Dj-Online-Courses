78c39d8daa2a63e8ae2220a2e742fc6dc7cbc7f9
unknown <mmohamedabdelm@gmail.com>
2024-04-05 21:49:49 +0200

    update in template home page

--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Company
-
+from courses.models import Course
 
 def home(request):
     data = Company.objects.last()
+    courses = Course.objects.all()[:5]
 
-    return render(request, 'home.html', {'data':data})
+    return render(request, 'home.html', {'data':data, 'courses':courses})
