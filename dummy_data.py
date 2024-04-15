c0197c7e8e715b542e95714ab00529b68559a00d
unknown <mmohamedabdelm@gmail.com>
2024-04-16 01:22:53 +0200

    add media files

--- dummy_data.py
+++ dummy_data.py
@@ -264,8 +264,8 @@ def seed_reviews(n):
 
     print(f"Seeded {n} Reviews Successfully")
 
-seed_courses(100)
+# seed_courses(100)
 # seed_units(100)
-# seed_lessons(10)
-# seed_reviews(60)
+# seed_lessons(100)
+# seed_reviews(200)
 # create_users(5)
