6cc37b48d38b44a48dde8c542f90000a3c123baa
unknown <mmohamedabdelm@gmail.com>
2024-04-21 17:07:12 +0200

    add dummy_data for quizes

--- dummy_data.py
+++ dummy_data.py
@@ -5,7 +5,7 @@ django.setup()
 
 from faker import Faker
 import random
-from courses.models import Course, Lesson, Unit, Review
+from courses.models import Course, Lesson, Unit, Review, Question, Quiz, Choice
 from django.contrib.auth.models import User
 from datetime import timedelta
 from taggit.models import Tag
@@ -59,8 +59,38 @@ def seed_courses(n):
 
 
 
+def seed_quizzes_and_questions(n):
+    fake = Faker()
+    units = Unit.objects.all()
 
+    for _ in range(n):
+        title = fake.sentence(nb_words=4)
+        duration = random.randint(10, 60)  # Assuming duration in minutes
+        unit = random.choice(units)
+        quiz = Quiz.objects.create(
+            unit=unit,
+            title=title,
+            duration=duration,
+        )
 
+        print(f"Seeded quiz: {quiz.title}")
+
+        # Create questions for the quiz
+        for _ in range(random.randint(3, 5)):  # You can adjust the number of questions as needed
+            question_text = fake.sentence(nb_words=10)  # Generate a random sentence for question text
+            question = Question.objects.create(quiz=quiz, text=question_text)
+
+            print(f"Seeded question: {question.text}")
+
+            # Create choices for each question
+            for _ in range(random.randint(2, 4)):  # You can adjust the number of choices as needed
+                choice_text = fake.sentence(nb_words=6)
+                is_correct = fake.boolean(chance_of_getting_true=30)  # Adjust probability of correct choice
+                choice = Choice.objects.create(question=question, text=choice_text, is_correct=is_correct)
+
+                print(f"Seeded choice: {choice.text}")
+
+    print(f"Seeded {n} Quizzes, Questions, and Choices Successfully")
 
 
 
@@ -98,6 +128,7 @@ def seed_lessons(n):
     units = Unit.objects.all()
     lesson_resources = ['1 (1).pdf', '1 (2).pdf', '1 (3).pdf', '1 (4).pdf']
     lesson_slides = ['1 (1).pdf', '1 (2).pdf', '1 (3).pdf', '1 (4).pdf', '1 (5).pdf', '1 (6).pdf']
+    videos = ['1 (1).mp4', '1 (2).mp4', '1 (3).mp4', '1 (4).mp4', '1 (5).mp4', '1 (6).mp4', '1 (7).mp4']
 
     for _ in range(n):
         title = fake.sentence(nb_words=4)
@@ -107,7 +138,7 @@ def seed_lessons(n):
         instructor = random.choice(users)
         course = random.choice(courses)
         unit = random.choice(units)
-        video = fake.file_path(depth=2, category='video')
+        video = f'course_videos/{random.choice(videos)}'
         resources = f'lesson_resources/{random.choice(lesson_resources)}'
         assignment = fake.boolean(chance_of_getting_true=50)
         slides = f'lesson_slides/{random.choice(lesson_slides)}'
@@ -140,114 +171,6 @@ def seed_lessons(n):
 
 
 
-
-# def seed_courses(n):
-#     fake = Faker('en_US')
-#     skill_level = ['Beginner', 'Intermediate', 'Advanced']
-#     language = ['Arabic', 'English', 'French']
-#     tags = ['python', 'html', 'programming', 'code', 'c', 'hello_world', 'beginner', 'basics']
-#     users = User.objects.all()
-
-#     images = ['1 (1).png', '1 (10).png', '1 (11).png', '1 (12).png', '1 (2).png', '1 (3).png', '1 (4).png', '1 (5).png', '1 (6).png', '1 (7).png', '1 (8).png', '1 (9).png', '1 (1).jpg', '1 (2).jpg', '1 (3).jpg', '1 (1).jpeg', '1 (10).jpeg', '1 (11).jpeg', '1 (12).jpeg', '1 (13).jpeg', '1 (14).jpeg', '1 (15).jpeg', '1 (2).jpeg', '1 (3).jpeg', '1 (4).jpeg', '1 (5).jpeg', '1 (6).jpeg', '1 (7).jpeg', '1 (8).jpeg', '1 (9).jpeg']
-    
-#     for _ in range(n):
-#         # Generate a random duration using timedelta
-#         duration_days = random.randint(1, 10)
-#         duration = timedelta(days=duration_days)
-
-#         # Choose a random user's ID
-#         random_user_id = fake.random_element(users).id
-
-#         # Choose random tags for the course
-#         num_tags = random.randint(1, 3)  # Choose between 1 to 3 tags per course
-#         chosen_tags = random.sample(tags, num_tags)
-
-#         course = Course.objects.create(
-#             title=fake.name(),
-#             user_id=random_user_id,
-#             image=f'course_images/{images[random.randint(0, 29)]}',
-#             price=round(random.uniform(20.99, 99.99), 2),
-#             skill_level=random.choice(skill_level), 
-#             duration=duration,
-#             rate=random.randint(0, 4),
-#             language=random.choice(language),
-#             description=fake.text(max_nb_chars=1000),
-#         )
-
-#         # Assign the chosen tags to the course
-#         course.tags.add(*chosen_tags)
-
-#     print(f"Seeded {n} Courses Successfully")
-
-
-# def seed_unit(n):
-#     fake = Faker()
-#     for _ in range(n):
-#         Unit.objects.create(
-#             title=fake.name(),
-#             Course =  Course.objects.get(id=random.randint(1,150)),
-
-#                             )
-#     print(f"Seeded {n} Unit Successfully")
-
-
-
-
-# def seed_product(n):
-#     fake = Faker()
-#     images = ['14.png', '36.png', '40.png', '5.png', '7.png', '70.png', '10.jpg', '19.jpg', 
-# '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '29.jpg', '37.jpg', '38.jpg', '39.jpg', '41.jpg', '42.jpg', '43.jpg', '44.jpg', '45.jpg', '46.jpg', '47.jpg', '48.jpg', '49.jpg', '5.jpg', '51.jpg', '52.jpg', '53.jpg', '54.jpg', '55.jpg', '56.jpg', '57.jpg', '58.jpg', '59.jpg', '6.jpg', '60.jpg', '61.jpg', '62.jpg', '63.jpg', '64.jpg', '65.jpg', '66.jpg', '67.jpg', '68.jpg', '69.jpg', '8.jpg', '9.jpg', '100.jpeg', '101.jpeg', '102.jpeg', '103.jpeg', '104.jpeg', '105.jpeg', '106.jpeg', '107.jpeg', '108.jpeg', '109.jpeg', '11.jpeg', '110.jpeg', '111.jpeg', '112.jpeg', '113.jpeg', '114.jpeg', '115.jpeg', '116.jpeg', '117.jpeg', '118.jpeg', '119.jpeg', '12.jpeg', '120.jpeg', '121.jpeg', '122.jpeg', '123.jpeg', '124.jpeg', '125.jpeg', '126.jpeg', '127.jpeg', '128.jpeg', '129.jpeg', '13.jpeg', '130.jpeg', '131.jpeg', '15.jpeg', '16.jpeg', '17.jpeg', '18.jpeg', '2.jpeg', '25.jpeg', '26.jpeg', '27.jpeg', '28.jpeg', '3.jpeg', '30.jpeg', '31.jpeg', '32.jpeg', '33.jpeg', '34.jpeg', '35.jpeg', '50.jpeg', '71.jpeg', '72.jpeg', '73.jpeg', '74.jpeg', '75.jpeg', '76.jpeg', '77.jpeg', '78.jpeg', '79.jpeg', '8.jpeg', '80.jpeg', '81.jpeg', '82.jpeg', '83.jpeg', '84.jpeg', '85.jpeg', '86.jpeg', '87.jpeg', '88.jpeg', '89.jpeg', '90.jpeg', '91.jpeg', '92.jpeg', '93.jpeg', '94.jpeg', '95.jpeg', '96.jpeg', '97.jpeg', '98.jpeg', '99.jpeg', '18.webp', '19.webp', '32.webp', '5.webp']
-#     flags = ['new', 'sale', 'feature']
-#     for _ in range(n):
-#         Product.objects.create(
-#             name = fake.name(),
-#             image = f'products/{images[random.randint(0,134)]}',
-#             flag = flags[random.randint(0, 2)],
-#             price = round(random.uniform(20.99, 99.99),2),
-#             sku = random.randint(1000,100000) ,
-#             rate = random.randint(0,4) ,
-#             subtitle = fake.text(max_nb_chars=250),
-#             description = fake.text(max_nb_chars=2000),
-#             quantity = random.randint(0,30),
-#             brand = Brand.objects.get(id=random.randint(1,20)),
-
-#         )
-
-#     print(f"Seed {n} Products Successfully")
-
-
-# def seed_product_images(n):
-#     fake = Faker()
-#     images = ['14.png', '36.png', '40.png', '5.png', '7.png', '70.png', '10.jpg', '19.jpg', 
-# '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '29.jpg', '37.jpg', '38.jpg', '39.jpg', '41.jpg', '42.jpg', '43.jpg', '44.jpg', '45.jpg', '46.jpg', '47.jpg', '48.jpg', '49.jpg', '5.jpg', '51.jpg', '52.jpg', '53.jpg', '54.jpg', '55.jpg', '56.jpg', '57.jpg', '58.jpg', '59.jpg', '6.jpg', '60.jpg', '61.jpg', '62.jpg', '63.jpg', '64.jpg', '65.jpg', '66.jpg', '67.jpg', '68.jpg', '69.jpg', '8.jpg', '9.jpg', '100.jpeg', '101.jpeg', '102.jpeg', '103.jpeg', '104.jpeg', '105.jpeg', '106.jpeg', '107.jpeg', '108.jpeg', '109.jpeg', '11.jpeg', '110.jpeg', '111.jpeg', '112.jpeg', '113.jpeg', '114.jpeg', '115.jpeg', '116.jpeg', '117.jpeg', '118.jpeg', '119.jpeg', '12.jpeg', '120.jpeg', '121.jpeg', '122.jpeg', '123.jpeg', '124.jpeg', '125.jpeg', '126.jpeg', '127.jpeg', '128.jpeg', '129.jpeg', '13.jpeg', '130.jpeg', '131.jpeg', '15.jpeg', '16.jpeg', '17.jpeg', '18.jpeg', '2.jpeg', '25.jpeg', '26.jpeg', '27.jpeg', '28.jpeg', '3.jpeg', '30.jpeg', '31.jpeg', '32.jpeg', '33.jpeg', '34.jpeg', '35.jpeg', '50.jpeg', '71.jpeg', '72.jpeg', '73.jpeg', '74.jpeg', '75.jpeg', '76.jpeg', '77.jpeg', '78.jpeg', '79.jpeg', '8.jpeg', '80.jpeg', '81.jpeg', '82.jpeg', '83.jpeg', '84.jpeg', '85.jpeg', '86.jpeg', '87.jpeg', '88.jpeg', '89.jpeg', '90.jpeg', '91.jpeg', '92.jpeg', '93.jpeg', '94.jpeg', '95.jpeg', '96.jpeg', '97.jpeg', '98.jpeg', '99.jpeg', '18.webp', '19.webp', '32.webp', '5.webp']
-#     for _ in range(n):
-#         ProductImages.objects.create(
-#             product = Product.objects.get(id=random.randint(0,90)),
-#             image = f'product_images/{images[random.randint(0,134)]}'
-#         )
-
-#     print(f"Seed {n} images in product Successfully")
-
-
-
-
-
-
-
-# def create_users(n):
-#     fake = Faker()
-#     for _ in range(n):
-#         User.objects.create_user(
-#             username=fake.user_name(),
-#             password=fake.password()
-#     )
-#     print(f"Seed {n} Reviews Successfully")
-    
-
-
-
-
-
 def seed_reviews(n):
     fake = Faker()
     users = User.objects.all()
@@ -264,8 +187,9 @@ def seed_reviews(n):
 
     print(f"Seeded {n} Reviews Successfully")
 
-# seed_courses(100)
-# seed_units(10)
-# seed_lessons(30)
-# seed_reviews(200)
+seed_courses(100)
+seed_units(300)
+seed_lessons(700)
+seed_reviews(700)
 # create_users(5)
+seed_quizzes_and_questions(300)
