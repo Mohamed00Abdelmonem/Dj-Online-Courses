768c792c8e23ddc1c197ae26dfd10d06f9407c3f
unknown <mmohamedabdelm@gmail.com>
2024-07-01 23:04:48 +0300

    add token auth

--- project/settings.py
+++ project/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+
 
 
 
@@ -84,9 +86,14 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+     
     ],
+
+    #   'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ], 
