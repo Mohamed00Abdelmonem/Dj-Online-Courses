a5c18f9e845d93937371fe09bac003f582c10168
unknown <mmohamedabdelm@gmail.com>
2024-07-06 20:53:09 +0300

    make design BD for order and order_detail and coupon

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Cart, CartDetial
+from .models import Cart, CartDetial, Order, OrderDetail, Coupon
 # Register your models here.
 
 admin.site.register(Cart)
 admin.site.register(CartDetial)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Coupon)
--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from courses.models import Course
+from utils.generate_code import generate_code
+from django.utils import timezone
+import datetime
+
+
+
 
 
 CART_STATUS = {
@@ -28,3 +34,49 @@ class CartDetial(models.Model):
             return f"cart_detail {self.cart}"
         else: 
             return f"Cart (no user)"
+        
+
+
+ORDER_STATUS = {
+    ("InProgress","InProgress"),
+    ("Completed", "Completed")
+}
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_order')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_order')
+    code = models.CharField(max_length=9 ,default=generate_code())
+    status = models.CharField(max_length=10, choices=ORDER_STATUS, default="InProgress")
+    coupon = models.ForeignKey('Coupon', related_name='order_coupon', on_delete=models.SET_NULL, null=True, blank=True)
+    total_after_coupon = models.FloatField(null=True,blank=True)
+    
+    def __str__(self):
+        return f'{self.user}'
+    
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_detail")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, related_name='order_course', null=True, blank=True)
+    price = models.FloatField()
+    total = models.FloatField(null=True, blank=True)
+    
+    def __str__(self):
+            return f'{self.order}'
+
+
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20)
+    discount = models.IntegerField()
+    quantity= models.IntegerField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.code}'
+
+    def save(self, *args, **kwargs):
+      week = datetime.timedelta(days=7)
+      self.end_date = self.start_date + week
+      super (Coupon, self).save(*args, **kwargs)
