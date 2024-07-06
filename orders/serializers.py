2b3bc97612cf7d2dca592b0ec85460192cecb408
unknown <mmohamedabdelm@gmail.com>
2024-07-06 20:24:53 +0300

    add course_cart_serilaizer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import Cart, CartDetial
+from courses.models import Course
 
-
+class CourseCartSeriaizer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['title','image', 'price']
 
 class CartDetailSeriaizer(serializers.ModelSerializer):
+    course = CourseCartSeriaizer()
     class Meta:
         model = CartDetial
         fields = '__all__'
