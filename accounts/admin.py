fb2744a332961925695696d01f8b5f63313ec47b
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-31 09:25:36 +0300

    update admin.py file for theme admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from unfold.admin import ModelAdmin
+
 # Register your models here.
 
 
-admin.site.register(Profile)
+admin.site.register(Profile, ModelAdmin)
--- courses/admin.py
+++ courses/admin.py
@@ -4,6 +4,43 @@ from unfold.admin import ModelAdmin
 
 
 
+class SimpleAdmin(ModelAdmin):
+    list_display = ('title', 'skill_level', 'price', 'avg_rate')
+
+    unfold = {
+        'sections': [
+            {
+                'label': 'Simple Chart',
+                'content': {
+                    'chart': {
+                        'type': 'bar',
+                        'data': {
+                            'labels': ['A', 'B', 'C'],
+                            'datasets': [
+                                {
+                                    'label': 'Simple Data',
+                                    'data': [5, 10, 15],
+                                    'backgroundColor': 'rgba(153, 102, 255, 0.2)',
+                                    'borderColor': 'rgba(153, 102, 255, 1)',
+                                    'borderWidth': 1,
+                                },
+                            ],
+                        },
+                    },
+                },
+            },
+        ]
+    }
+
+admin.site.register(Course, SimpleAdmin)
+
+
+
+
+
+
+
+
 class LessonAdmin(ModelAdmin):
     list_display = ('id', 'title', 'course')
     list_filter = ('course',)  # Add filter for course
@@ -19,7 +56,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(ModelAdmin):
     inlines = [ChoiceInline]
 
-admin.site.register(Course)
+# Register the remaining models
 admin.site.register(Unit, UnitAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Quiz)
